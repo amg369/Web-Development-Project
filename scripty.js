d09b2f1017d27d24c68c0872fd033a869e155c80
Aliyah Geer <43519256+amg369@users.noreply.github.com>
2020-07-06 11:04:46 -0400

    Add files via upload

--- scripty.js
+++ scripty.js
@@ -1,20 +1,52 @@
 
+
 $(document).ready(function(){
     console.log("loaded");
     $.material.init();
 
-    $(document).on("submit","#register-form", function(e){
-        e.preventDefault();
-
-
+    $(document).on("submit", "#register-form", function(e){
+        e.preventDefault()
         var form = $('#register-form').serialize();
         $.ajax({
-                url: '/postregistration',
-                type: 'POST',
-                data: form,
-                success: function(response){
-                    console.log(response);
-                 }
+            url : '/postregistration',
+            type: 'POST',
+            data: form,
+            success: function(response){
+                console.log(response);
+            }
         });
+
+    $(document).on('submit','#login-form',function(e){
+        e.preventDefault();
+
+        var form =$(this).serialize();
+        $.ajax({
+            url: '/check-login',
+            type: 'POST',
+            data: form,
+            success: function(res){
+                if (res== "error"){
+                    alert("Could not log in.");
+                }else{
+                    console.log("Logged in as", res);
+                    window.location.href = "/";
+                    }
+            }
+
+        })
+    )};
+
+    $(document).on('click','#logout-link',function(e)){
+        e.preventDefault();
+        $.ajax({
+            url: '/logout',
+            type: 'GET',
+            success: function(response){
+                if (response == 'success'){
+                    window.location.href = '/login';
+                }else{
+                alert("Something went wrong.")
+            }
+        })
     });
 });
