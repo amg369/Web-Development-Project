1330c71a2f2dbb9ce4a37d7fe3e804ad955c6942
Aliyah Geer <43519256+amg369@users.noreply.github.com>
2020-07-06 11:12:22 -0400

    Add files via upload

--- Controller.py
+++ Controller.py
@@ -1,20 +1,25 @@
 import cheroot
 from webpy import web
-from Models import RegisterModel
+from Models import RegisterModel, LoginModel
 from pymongo import MongoClient
 
+web.config.debug = False
 
 urls = (
-    "/", 'Home', '/register', 'Register', '/postregistration', 'PostRegistration'
+    "/", 'Home',
+    '/register', 'Register',
+    '/login', 'Login',
+    '/logout', 'Logout',
+    '/postregistration', 'PostRegistration',
+    '/check-login', 'CheckLogin'
 )
 
-render = web.template.render("Views/Templates", base="MainLayout")
 app = web.application(urls, globals())
+session = web.session.Session(app, web.session.DiskStore("sessions"), initializer={"user": None})
+session_data = session._initializer
 
-
-class Register:
-    def GET(self):
-        return render.Register()
+render = web.template.render("Views/Templates", base="MainLayout",
+                             globals={'session': session_data, 'current_user': session_data["user"]})
 
 
 # Classes/Routes
@@ -23,12 +28,44 @@ class Home:
         return render.Home()
 
 
+class Register:
+    def GET(self):
+        return render.Register()
+
+
+class Login:
+    def GET(self):
+        return render.Login()
+
+
+class CheckLogin:
+    def POST(self):
+        data = web.input()
+        login = LoginModel.LoginModel()
+        isCorrect = login.check_user(data)
+
+        if isCorrect:
+            session_data["user"] = isCorrect
+            return isCorrect
+
+        return "error"
+
+
 class PostRegistration:
     def POST(self):
         data = web.input()
         reg_model = RegisterModel.RegisterModel()
-        reg_model.insert_user(data)
+        reg_model.add_user(data)
+
         return data.username
+
+
+class Logout:
+    def GET(self):
+        session['user'] == None
+        session_data['user'] = None
+        session.kill()
+        return "success"
 
 
 if __name__ == "__main__":
